d8016f7392e2f40125b8af927619d1b9397a1485
El <elhabib10benaboud@gmail.com>
2024-11-15 19:28:47 +0100

    fixture

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $categories = ['Electronics', 'Books', 'Clothing', 'Home Appliances'];
+
+        foreach ($categories as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
