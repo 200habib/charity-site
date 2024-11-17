98e3fb206df6e4afe1390ca23ce777bd91617a45
El <elhabib10benaboud@gmail.com>
2024-11-17 20:19:14 +0100

    style button

--- assets/javascript/filtersize.js
+++ assets/javascript/filtersize.js
@@ -1,20 +1,19 @@
 const layoutToggle1x1 = document.getElementById('layout-toggle-1x1');
 const layoutToggle1x2 = document.getElementById('layout-toggle-1x2');
 const productList = document.querySelector('.productList');
-const AddToBasket = document.querySelector('.card-img-top');
+const AddToBasket = document.querySelectorAll('.card-img-top');
 
-productList.classList.remove('active'); 
-AddToBasket.classList.remove('active'); 
-
+productList.classList.remove('active');
+AddToBasket.forEach(button => button.classList.remove('active'));
 
 layoutToggle1x1.addEventListener('click', function(event) {
     event.preventDefault();
     productList.classList.add('active');
-    AddToBasket.classList.add('active');
+    AddToBasket.forEach(button => button.classList.add('active'));
 });
 
 layoutToggle1x2.addEventListener('click', function(event) {
     event.preventDefault();
     productList.classList.remove('active');
-    AddToBasket.classList.remove('active');
+    AddToBasket.forEach(button => button.classList.remove('active'));
 });
